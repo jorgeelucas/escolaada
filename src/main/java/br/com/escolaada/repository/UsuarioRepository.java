918266774cc6610f3547d7924ecfb6180e75a792
Jorge Lucas <jorge.santos@bitso.com>
2022-11-01 02:02:54 -0300

    criando usuário repository

---
+++ src/main/java/br/com/escolaada/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package br.com.escolaada.repository;
+
+import br.com.escolaada.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+}
