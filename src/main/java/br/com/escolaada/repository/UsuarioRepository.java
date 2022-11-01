b54f0b2520df5c042e1252fefb00f89208e6a6bf
Jorge Lucas <jorge.santos@bitso.com>
2022-11-01 02:22:03 -0300

    criando userDetailsServiceImpl e atualizando o repository

--- src/main/java/br/com/escolaada/repository/UsuarioRepository.java
+++ src/main/java/br/com/escolaada/repository/UsuarioRepository.java
@@ -4,6 +4,11 @@ import br.com.escolaada.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+
+    Optional<Usuario> findUsuarioByUsuario(String usuario);
+
 }
