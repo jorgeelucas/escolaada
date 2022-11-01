394570e3b48ebfaea8cff8f84b42d0e07fb71471
Jorge Lucas <jorge.santos@bitso.com>
2022-11-01 02:01:56 -0300

    criando entidade Usuario

---
+++ src/main/java/br/com/escolaada/entity/Usuario.java
@@ -0,0 +1,66 @@
+package br.com.escolaada.entity;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.UUID;
+
+@Entity
+@Data
+public class Usuario implements UserDetails, Serializable { // implements UserDetails para avisar ao spring
+    private static final long serialVersionUID = 1;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id; // tipo mais apropriado para sistemas distribuidos
+
+    @Column(nullable = false, unique = true)
+    private String usuario;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // cargos e roles
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.usuario;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
