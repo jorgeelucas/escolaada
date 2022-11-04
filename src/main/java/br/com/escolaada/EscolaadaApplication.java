81bcf249daf74609aac6c5deb846c20e2645fe60
Jorge Lucas <jorge.santos@bitso.com>
2022-11-04 12:54:46 -0300

    configurando o uso das keys

--- src/main/java/br/com/escolaada/EscolaadaApplication.java
+++ src/main/java/br/com/escolaada/EscolaadaApplication.java
@@ -1,8 +1,11 @@
 package br.com.escolaada;
 
+import br.com.escolaada.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class EscolaadaApplication {
 
---
+++ src/main/java/br/com/escolaada/config/RsaKeyProperties.java
@@ -0,0 +1,10 @@
+package br.com.escolaada.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+@ConfigurationProperties("rsa")
+public record RsaKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
+}
