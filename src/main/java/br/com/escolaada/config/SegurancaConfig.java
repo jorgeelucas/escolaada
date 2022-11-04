8ea4838b66653843c0eebfad435de60e99019bbb
Jorge Lucas <jorge.santos@bitso.com>
2022-11-04 12:45:38 -0300

    trocando de httpbasic para oauth2

--- src/main/java/br/com/escolaada/config/SegurancaConfig.java
+++ src/main/java/br/com/escolaada/config/SegurancaConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -34,7 +35,7 @@ public class SegurancaConfig {
                     auth.antMatchers("/h2-console/**").permitAll();
                     auth.anyRequest().authenticated();
                 })
-                .httpBasic(Customizer.withDefaults())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .sessionManagement(sessionManagerCustomizer ->
                         sessionManagerCustomizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
