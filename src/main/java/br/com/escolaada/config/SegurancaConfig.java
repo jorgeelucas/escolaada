a032b0a682b27c46acfb09fee34fa9d85dc7480e
Jorge Lucas <jorge.santos@bitso.com>
2022-11-04 12:55:07 -0300

    criando encoder e decoder

--- src/main/java/br/com/escolaada/config/SegurancaConfig.java
+++ src/main/java/br/com/escolaada/config/SegurancaConfig.java
@@ -1,22 +1,32 @@
 package br.com.escolaada.config;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SegurancaConfig {
+
+    private final RsaKeyProperties rsaKey;
+
+    public SegurancaConfig(RsaKeyProperties rsaKey) {
+        this.rsaKey = rsaKey;
+    }
 
     @Bean
     public SecurityFilterChain configuracao(HttpSecurity http) throws Exception {
@@ -40,6 +50,22 @@ public class SegurancaConfig {
                         sessionManagerCustomizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
 
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(rsaKey.publicKey()).build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(rsaKey.publicKey())
+                .privateKey(rsaKey.privateKey())
+                .build();
+
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+
+        return new NimbusJwtEncoder(jwks);
     }
 
 }
